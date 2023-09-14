dce8d21b89dbced347c87e31beff78dcfe4d2d6c
hjkim0905 <eax9952@gmail.com>
2023-09-14 18:46:58 +0900

    <body>태그 삭제

--- ot_homework_hyunjoongkim/src/App.js
+++ ot_homework_hyunjoongkim/src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <body>                              {/*배경색 추가를 위한 <body>태그 입력*/}
     <div className="App">
-      <div className="navbar">          {/*상단에 내비게이션 바를 위치하여 페이지의 제목 입력*/}
+      <div className='navbar'>    {/*상단에 내비게이션 바를 위치하여 페이지의 제목 입력*/}
         <div>
           현중's 자기소개
         </div>
       </div>
     </div>
-    </body>
   );
 }
 
