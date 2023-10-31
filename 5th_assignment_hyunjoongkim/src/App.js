55ef0434a51bf4df480146d3f071d37f4fc4cec9
hjkim0905 <eax9952@gmail.com>
2023-11-01 00:02:32 +0900

    Context/ Provider 생성 및 useContext 사용
    좋아요 상태, 댓글 상태를 전역처리하여 관리해서 컴포넌트 이동간에도 데이터가 사라지지 않고 저장

--- 5th_assignment_hyunjoongkim/src/App.js
+++ 5th_assignment_hyunjoongkim/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyPage from "./MyPage";
 import EditProfile from "./EditProfile";
 import Home from "./Home";
+import HomeContext from "./HomeContext"; //생성한 Context 불러오기
 
 function App() {
   const [profileData, setProfileData] = useState({
@@ -13,22 +14,39 @@ function App() {
     gender: "Male",
   });
 
+  // likeStatus, likeCount, commentsList 초기 상태 선언
+  const [likeStatus, setLikeStatus] = useState(false);
+  const [likeCount, setLikeCount] = useState(1070);
+  const [commentsList, setCommentsList] = useState([]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MyPage profileData={profileData} />} />
-        <Route
-          path="/EditProfile"
-          element={
-            <EditProfile
-              profileData={profileData}
-              setProfileData={setProfileData}
-            />
-          }
-        />
-        <Route path="/Home" element={<Home profileData={profileData} />} />
-      </Routes>
-    </Router>
+    //좋아요 수, 댓글 값의 상태를 value로 포함한 Context Provider로 선언 및 하위 컴포넌트들 묶기
+    <HomeContext.Provider
+      value={{
+        likeStatus,
+        setLikeStatus,
+        likeCount,
+        setLikeCount,
+        commentsList,
+        setCommentsList,
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<MyPage profileData={profileData} />} />
+          <Route
+            path="/EditProfile"
+            element={
+              <EditProfile
+                profileData={profileData}
+                setProfileData={setProfileData}
+              />
+            }
+          />
+          <Route path="/Home" element={<Home profileData={profileData} />} />
+        </Routes>
+      </Router>
+    </HomeContext.Provider>
   );
 }
 
